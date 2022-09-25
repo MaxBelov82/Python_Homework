6b16ad95d16c8d029dedce054fdc7729c36fecf0
Maxim Belov <belov.max7@yandex.ru>
2022-09-25 21:06:27 +0300

    Сделал задачу 4

--- Task9/Task9.py
+++ Task9/Task9.py
@@ -68,5 +68,155 @@
 # quantity_mandarins = int(input())
 # print(quantity_mandarins//quantity_pupils)
 # print(quantity_mandarins%quantity_pupils)
-Population_n = int(input())
-print(Population_n-Population_n//2)
+# Population_n = int(input())
+# print(Population_n-Population_n//2)
+# num_minutes = int(input())
+# print(num_minutes//60,'час',num_minutes%60, 'мин.')
+# three_digit_num = int(input())
+# print('Сумма цифр', '=',three_digit_num//100+(three_digit_num%100//10)+three_digit_num%10)
+# print('Сумма цифр', '=',three_digit_num//100*(three_digit_num%100//10)*(three_digit_num%10))
+# print('Поэма "Мёртвые души" одна из самых интересных')
+# print("Python", "is the best")
+# print("3.1415")
+# print("I'm a math teacher and a programmer!")
+# print()
+# print("The world's a little blurry", "Or maybe it's my eyes", end='!!!', sep=' :) ')
+# print("Honey, what's your hurry", end='?')
+# print("The world's a little blurry", "Or maybe it's my eyes", end='!!!', sep=' :) ')
+# print("Told you not to worry", "But maybe that's a lie", sep=' :) ')
+# s = 13
+# k = -5
+# d = s + 2
+# s = d
+# k = 2 * s
+# print(s + k + d)
+# a = 17 // (23 % 7)
+# b = 34 % a * 5 - 29 % 4 * 3
+# print(a * b)
+# print("Told you not to worry", "But maybe that's a lie", sep=' :) ')
+# print("Honey, what's your hurry", end='?')
+# print("The world's a little blurry", "Or maybe it's my eyes", end='!!!', sep=' :) ')
+# print('Поэма "Мёртвые души" одна из самых интересных')
+# print("I'm a math teacher and a programmer!")
+# print('Просто текст...')
+# print()
+# print("3.1415")
+# print("I'm a math teacher and a programmer!")
+# print('I'm 16 and I'm from Northern Ireland.')
+# print('*'*17,end='\n\n')
+# print('*','*',sep=' '*15,end='\n')
+# print('*'*17,end='\n\n')
+# print('*','*',sep=' '*15,end='\n')
+# num1, num2 = int(input()),int(input())
+# print('Квадрат суммы',num1,'и',num2,'равен',(num1+num2)**2)
+# num_n = int(input())
+# print(num_n+num_n*num_n+num_n*num_n*num_n)
+# i = int(input())
+# if i // 2 != 0:
+#     print(i, 'нечётное')
+# else:
+#     print(i, 'чётное')
+# four_digit_num = int(input())
+# sum_digit1 = four_digit_num//1000+four_digit_num%10
+# sum_digit2 = four_digit_num//100%10-four_digit_num%100//10
+# if sum_digit1 == sum_digit2:
+#     print('ДА')
+# else:
+#     print('НЕТ')
+# num = int(input())
+# print(num//100%10-num%100//10)
+# num1,num2,num3 = int(input()),int(input()),int(input())
+# if (num2-num1)+num2 == num3:
+#     print('YES')
+# else:
+#     print('NO')
+# num1,num2 = int(input()),int(input())
+# if num1 >num2:
+#    print(num1)
+# if num2 >num1:
+#    print(num2)
+# num1,num2,num3,num4 = int(input()),int(input()),int(input()),int(input())
+# if num1<num2:
+#    num2 = num1
+# if num3<num4:
+#    num4 = num3
+# if num1<num3:
+#     print(num1)
+# else:
+#     print(num3)
+# num1,num2,num3,num4 = int(input()),int(input()),int(input()),int(input())
+# if num1<num2:
+#     min_a = num1
+# if num3<num4:
+#     min_b = num3
+# if min_a<min_b:
+#     print(min_a)
+# else:
+#     print(min_b)   
+
+# Напишите программу, которая принимает на вход 2 числа и определяет
+# является ли одно число квдратом другого
+# num1,num2 = int(input('Введите первое число:')),int(input('Введите второе число:'))
+# if num1 == num2**2 or num2 == num1**2:
+#     print('да')
+# else:
+#     print('нет')
+
+# Напишите программу, которая принимает на вход 5 чисел
+# и находит максимальное из них.
+# max = -1
+# for i in range(5):
+#     n = int(input('Введите пять чисел:''\n'))
+#     if max < n:
+#        max = n
+#     print(max)
+# Напишите программу, которая будет принимать на вход дробь 
+# и показывать первую цифру дробной части числа.
+# *Примеры:*
+# -6,78 ->7
+# -5 -> нет
+# -0,34 -> 3
+
+# real_n = float(input())
+# print(int(real_n*10)%10)
+
+# Напишите программу, которая принимает на вход число и 
+# проверяет, кратно ли оно 5 и 10 или 15, но не 30
+# num = int(input())
+# if (num%5 == 0 and num%10 == 0) or (num%15 == 0 and num%30 !=0):
+#    print('Кратно')
+# else:
+#    print('не кратно')
+
+# Для натурального n создать словарь индекс-значение, состоящее из элементов
+# последовательности 3n + 1,
+# *Пример:*
+# - Для n = 6: {1:4,2:7,3:10, 4: 13, 5: 16, 6: 19}
+
+d = {}  # создал словарь
+d = dict()
+d ['Дмитрий'] = '88003332211'
+print(d)
+
+# age_num = int(input('Введите свой возраст:'))
+# if age_num <= 13:
+#     print('детство')
+# elif age_num >= 14 and age_num<= 24:
+#     print('молодость')
+# elif age_num >= 25 and age_num<= 59:
+#     print('зрелость')
+# elif age_num >= 60:
+#     print('старость')
+
+# a,b,c = int(input()),int(input()),int(input())
+# if a > 0 and b > 0 and c > 0:
+#     print(a+b+c)
+# elif a < 0:
+#     print(b+c)
+# elif b < 0:
+#     print(a+c)
+# elif c < 0:
+#     print(a+b)
+# elif a < 0 and b < 0 and c < 0:
+#     print(0)
+
